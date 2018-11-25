1e1a6274553a43390d92cb69d988627e4b70d31f
Daniela <t.dani@live.com>
2018-11-25 12:51:34 -0800

    keep hydrated

---
+++ katas/keepHydrated.js
@@ -0,0 +1,14 @@
+const Test = require('../Test');
+
+function litres(time) {
+    return Math.floor(time * .5);
+  }
+
+
+ Test.assertEquals(litres(2), 1, 'should return 1 litre');
+    Test.assertEquals(litres(1.4), 0, 'should return 0 litres');
+    Test.assertEquals(litres(12.3), 6, 'should return 6 litres');
+    Test.assertEquals(litres(0.82), 0, 'should return 0 litres');
+    Test.assertEquals(litres(11.8), 5, 'should return 5 litres');
+    Test.assertEquals(litres(1787), 893, 'should return 893 litres');
+    Test.assertEquals(litres(0), 0, 'should return 0 litres');
